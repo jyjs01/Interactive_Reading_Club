0e61080c088382ac9c71e82c5628ad932477c94f
JY <duawodud12@naver.com>
2024-07-17 21:40:09 +0900

    Revert "[frontend] 비로그인 페이지 NonLogin 컴포넌트 구현"
    This reverts commit 9a02a07c71df88fe0c2d2ef1ceee2cc226e63d14.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { useEffect } from 'react';
-import NonLogin from './components/NonLogin';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  // useEffect(() => {
-  //   fetch('http://localhost:4000/')
-  //     .then(res => {
-  //       if (!res.ok) {
-  //         throw new Error('Network response was not ok');
-  //       }
-  //       return res.json();
-  //     })
-  //     .then(data => {
-  //       console.log(data);
-  //     })
-  //     .catch(error => {
-  //       console.error('Fetch error:', error);
-  //     });
-  // }, []);
-
   return (
-    <div>
-      <NonLogin />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
